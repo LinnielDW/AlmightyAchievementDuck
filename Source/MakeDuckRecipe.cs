77094912b714ad377e72d667467363a13d579b1a
Linniel Driver-Williams <LinnielDW@googlemail.com>
2025-03-23 21:02:07 +0100

    changed recipe logic

--- Source/MakeDuckRecipe.cs
+++ Source/MakeDuckRecipe.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using System.Linq;
 using AAD.Settings;
-using RimWorld;
 using Verse;
 
 namespace AAD;
@@ -11,45 +9,28 @@ public static class MakeDuckRecipe
 {
     static MakeDuckRecipe()
     {
-        var allTheIngredients = new List<IngredientCount>();
-
-        // var exclusions = settings.excludedCats.NullOrEmpty()
-        //     ? defaultExclusions
-        //     : settings.excludedCats;
-        //
-        // var inclusions = settings.includedCats.NullOrEmpty()
-        //     ? defaultInclusions
-        //     : settings.includedCats;
-
         var allThingDefs = DefDatabase<ThingDef>.AllDefsListForReading;
+
+        // foreach (var t in allThingDefs.Where(t => t.thingCategories is { Count: > 1 }))
+        // {
+        //     Log.Warning(t.defName + ": " + t.thingCategories.ToStringSafeEnumerable());
+        // }
+            
         foreach (var thingDef in allThingDefs.Where(t =>
-                     t.PlayerAcquirable && t.selectable &&
-                     ((t.CountAsResource || ThingCategoryDefOf.Items
-                          .ContainedInThisOrDescendant(t))
-                      && 
-                      t.thingCategories.Any(cat => AlmightyAchievementDuckSettings.GetOrCreateUseCategorySetting(cat.defName) && cat.ContainedInThisOrDescendant(t))
-                      ) && t != DuckDefOf.AAD_AlmightyAchievementDuck
+                     t.PlayerAcquirable && 
+                     t.selectable &&
+                     t.category == ThingCategory.Item && 
+                     t.thingCategories != null && 
+                     t.thingCategories.Any(cat => 
+                         AlmightyAchievementDuckSettings.GetOrCreateUseCategorySetting(cat.defName)) && 
+                     t != DuckDefOf.AAD_AlmightyAchievementDuck
                  ))
         {
             var ingredient = new IngredientCount();
-            ingredient.filter.SetAllow(thingDef, true);
             ingredient.SetBaseCount(1);
-
-            allTheIngredients.Add(ingredient);
+            ingredient.filter.SetAllow(thingDef, true);
+            DuckDefOf.AAD_Make_AlmightyAchievementDuck.ingredients.Add(ingredient);
+            DuckDefOf.AAD_Make_AlmightyAchievementDuck.fixedIngredientFilter.SetAllow(thingDef, true);
         }
-
-        DuckDefOf.AAD_Make_AlmightyAchievementDuck.ingredients = allTheIngredients;
-
-        //TODO: refactor this:
-        // foreach (var category in DefDatabase<ThingCategoryDef>.AllDefsListForReading.Where(_ => !exclusions.Union(inclusions).Any(c => c.ThisAndChildCategoryDefs.Contains(c))))
-        // {
-            // Log.Error(category.defName + " was not found in recipe exclude or inclusion list");
-        // }
-
-
-        // foreach (var ing in DuckDefOf.Make_AlmightyAchievementDuck.ingredients)
-        // {
-        //     Log.Message(ing.ToStringSafe());
-        // }
     }
 }
